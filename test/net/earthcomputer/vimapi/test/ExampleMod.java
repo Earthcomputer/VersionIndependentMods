79f54a10bad3f3b408de516189e3008295521905
Earthcomputer <burtonjae@hotmail.co.uk>
2016-06-14 08:12:51 +0100

    Test for NBT interface

--- test/net/earthcomputer/vimapi/test/ExampleMod.java
+++ test/net/earthcomputer/vimapi/test/ExampleMod.java
@@ -5,6 +5,8 @@ import net.earthcomputer.vimapi.LifecycleEventType;
 import net.earthcomputer.vimapi.LifecycleHandler;
 import net.earthcomputer.vimapi.Registry;
 import net.earthcomputer.vimapi.VimMod;
+import net.earthcomputer.vimapi.nbt.NBTCompound;
+import net.earthcomputer.vimapi.nbt.NBTList;
 
 @VimMod(id = "example", version = "1.0")
 public class ExampleMod {
@@ -17,6 +19,25 @@ public class ExampleMod {
 				"minecraft:stick");
 		Registry.addShapelessRecipe(new ItemStack("minecraft:cobblestone", 0, 4), "minecraft:stone", "minecraft:stone",
 				"minecraft:stone", "minecraft:stone");
+		Registry.addShapedRecipe(new ItemStack("minecraft:diamond", 0, 2), "lll", "l l", "lll", 'l', "minecraft:log");
+		Registry.addShapedRecipe(new ItemStack("minecraft:diamond", 0, 2), "lll", "l l", "lll", 'l', "minecraft:log2");
+		NBTCompound nbt = new NBTCompound();
+		NBTList ench = new NBTList();
+		nbt.set("ench", ench);
+		NBTCompound enchantment = new NBTCompound();
+		enchantment.setInt("id", 34); // unbreaking
+		enchantment.setInt("lvl", 2);
+		ench.add(enchantment);
+		enchantment = new NBTCompound();
+		enchantment.setInt("id", 35); // fortune
+		enchantment.setInt("lvl", 2);
+		ench.add(enchantment);
+		enchantment = new NBTCompound();
+		enchantment.setInt("id", 16); // sharpness
+		enchantment.setInt("lvl", 2);
+		ench.add(enchantment);
+		Registry.addShapedRecipe(new ItemStack("minecraft:diamond_sword", 0, 1, nbt), "ddd", "ddd", "sss", 'd',
+				"minecraft:diamond", 's', "minecraft:stick");
 	}
 
 }
